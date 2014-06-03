e75382bb0847f214d98ce1b6f9ae60faeccf201a
Souheil CHELFOUH <trollfot@gmail.com>
2014-06-03 12:59:05 +0200

    Added IContent in the __init__ to expose it publicly

--- src/uvc/content/__init__.py
+++ src/uvc/content/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .interfaces import IDescriptiveSchema
+from .interfaces import IContent, IDescriptiveSchema
 from .directives import schema, Fields
 from .utils import bootstrap_component, schematic_bootstrap
